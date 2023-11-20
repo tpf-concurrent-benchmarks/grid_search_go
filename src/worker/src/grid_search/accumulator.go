f63940f9ec32ec471c256c17bd5be5c235f35a27
Facundo Mastricchio <facundomastricchio@gmail.com>
2023-11-20 20:02:30 -0300

    Add accumulator and grid_search

---
+++ src/worker/src/grid_search/accumulator.go
@@ -0,0 +1,59 @@
+package grid_search
+
+import (
+	"math"
+)
+
+type Accumulator struct {
+	callback   Callback
+	trueResult float64
+	trueInput  [Size]float64
+}
+
+type Callback func(float64, [Size]float64)
+
+func NewAccumulator(accumType string) *Accumulator {
+	acc := &Accumulator{}
+	switch accumType {
+	case "MAX":
+		acc.callback = acc.max
+		acc.trueResult = math.Inf(-1)
+	case "MIN":
+		acc.callback = acc.min
+		acc.trueResult = math.Inf(1)
+	default:
+		acc.callback = acc.avg
+		acc.trueResult = 0
+	}
+	return acc
+}
+
+func (acc *Accumulator) accumulate(res float64, current [Size]float64) {
+	acc.callback(res, current)
+}
+
+func (acc *Accumulator) getResult() float64 {
+	return acc.trueResult
+}
+
+func (acc *Accumulator) getInput() [Size]float64 {
+	return acc.trueInput
+}
+
+func (acc *Accumulator) max(res float64, current [Size]float64) {
+	if res > acc.trueResult {
+		acc.trueResult = res
+		copy(acc.trueInput[:], current[:])
+	}
+}
+
+func (acc *Accumulator) min(res float64, current [Size]float64) {
+	if res < acc.trueResult {
+		acc.trueResult = res
+		copy(acc.trueInput[:], current[:])
+	}
+}
+
+func (acc *Accumulator) avg(res float64, current [Size]float64) {
+	acc.trueResult += res
+}
