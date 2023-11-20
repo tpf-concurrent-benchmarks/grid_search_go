12ec7f75a02d9c8b4b625155909d85c1c64d92c0
Facundo Mastricchio <facundomastricchio@gmail.com>
2023-11-20 18:45:27 -0300

    Add interval

---
+++ src/manager/src/interval/interval.go
@@ -0,0 +1,79 @@
+package interval
+
+import (
+	"fmt"
+	"math"
+)
+
+type Interval struct {
+	start     float64
+	end       float64
+	step      float64
+	size      int
+	precision int
+}
+
+func NewInterval(start, end, step float64) *Interval {
+	size := int(math.Ceil((end - start) / step))
+	return &Interval{
+		start:     start,
+		end:       end,
+		step:      step,
+		size:      size,
+		precision: 10,
+	}
+}
+
+func (interval *Interval) Split(nPartitions int) []Interval {
+
+	if nPartitions <= 0 {
+		return nil
+	}
+
+	if interval.size%nPartitions == 0 {
+		return interval.splitEvenly(nPartitions)
+	}
+	maxElemsPerInterval := int(math.Ceil(float64(interval.size) / float64(nPartitions)))
+	nSubIntervalsFull := int(math.Floor(float64(interval.size-nPartitions) / float64(maxElemsPerInterval-1)))
+	var intervals []Interval
+	var subEnd float64
+	for j := 0; j < nSubIntervalsFull; j++ {
+		subStart := roundFloat(interval.start+float64(j*maxElemsPerInterval)*interval.step, float64(interval.precision))
+		subEnd := roundFloat(math.Min(interval.end, subStart+float64(maxElemsPerInterval)*interval.step), float64(interval.precision))
+		intervals = append(intervals, Interval{start: subStart, end: subEnd, step: interval.step, size: int((subEnd - subStart) / interval.step), precision: interval.precision})
+	}
+	intervalReminder := NewInterval(subEnd, interval.end, interval.step)
+	subIntervalsReminder := intervalReminder.Split(nPartitions - nSubIntervalsFull)
+	intervals = append(intervals, subIntervalsReminder...)
+	return intervals
+}
+
+func (interval *Interval) splitEvenly(nPartitions int) []Interval {
+	var intervals []Interval
+	for j := 0; j < nPartitions; j++ {
+		subStart := roundFloat(interval.start+float64(j*int(interval.size)/nPartitions)*interval.step, float64(interval.precision))
+		subEnd := roundFloat(interval.start+float64((j+1)*int(interval.size)/nPartitions)*interval.step, float64(interval.precision))
+		intervals = append(intervals, Interval{start: subStart, end: subEnd, step: interval.step, size: int((subEnd - subStart) / interval.step), precision: interval.precision})
+	}
+	return intervals
+}
+
+func (interval *Interval) print() {
+	fmt.Printf("start: %v, end: %v, step: %v\n", interval.start, interval.end, interval.step)
+}
+
+func (interval *Interval) intervalSize() int {
+	return interval.size
+}
+
+func (interval *Interval) getInterval() (float64, float64, float64) {
+	return interval.start, interval.end, interval.step
+}
+
+func (interval *Interval) setPrecision(precision int) {
+	interval.precision = precision
+}
+
+func roundFloat(value, precision float64) float64 {
+	return math.Round(value*math.Pow(10, precision)) / math.Pow(10, precision)
+}
