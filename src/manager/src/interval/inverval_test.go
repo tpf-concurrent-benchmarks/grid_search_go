22410e7b0a1d76cb18cea58524c0d1556a0a08be
ArmandoCivini <acivini@fi.uba.ar>
2023-11-23 22:56:12 -0300

    added additional tests to interval (1 failing test)

--- src/manager/src/interval/inverval_test.go
+++ src/manager/src/interval/inverval_test.go
@@ -1,7 +1,6 @@
 package interval
 
 import "testing"
-import "fmt"
 
 func compareIntervals(a *Interval, b *Interval) bool {
 	return a.start == b.start && a.end == b.end && a.step == b.step && a.size == b.size && a.precision == b.precision
@@ -11,6 +10,7 @@ func testIntervalGeneric(t *testing.T, interval *Interval, expected []Interval, 
 	actual := interval.Split(nPartitions)
 	if len(actual) != len(expected) {
 		t.Errorf("Split(%v) = %v; want %v", nPartitions, actual, expected)
+		return
 	}
 	for i := range actual {
 		if !compareIntervals(&actual[i], &expected[i]) {
@@ -27,10 +27,28 @@ func TestIntervalSplit(t *testing.T) {
 		nPartitions uint64
 	}{
 		{
-			name:       "Test Case 1",
+			name:       "even split with whole positive numbers", 
 			interval:   NewInterval(0, 10, 1),
 			expected:   []Interval{*NewInterval(0, 5, 1), *NewInterval(5, 10, 1)},
 			nPartitions: 2,
+		},
+		{
+			name:       "even split with whole negative numbers", 
+			interval:   NewInterval(-10, 10, 1),
+			expected:   []Interval{*NewInterval(-10, 0, 1), *NewInterval(0, 10, 1)},
+			nPartitions: 2,
+		},
+		{
+			name:       "even split with whole negative numbers odd split amount",
+			interval:   NewInterval(-600, 600, 1),
+			expected:   []Interval{*NewInterval(-600, -200, 1), *NewInterval(-200, 200, 1), *NewInterval(200, 600, 1)},
+			nPartitions: 3,
+		},
+		{
+			name:       "uneven split", 
+			interval:   NewInterval(0, 10, 3),
+			expected:   []Interval{*NewInterval(0, 6, 3), *NewInterval(6, 9, 3), *NewInterval(9, 12, 3)},
+			nPartitions: 3,
 		},
 	}
 
