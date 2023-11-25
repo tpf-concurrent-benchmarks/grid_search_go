6bbfc66f2ee1a3cee181ca980445377b53a15458
Facundo Mastricchio <facundomastricchio@gmail.com>
2023-11-25 00:38:01 -0300

    Add basic example of NATS
    - Not working on my end though lol, it doesn't even crash

--- src/manager/src/main.go
+++ src/manager/src/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/nats-io/nats.go"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	nc, err := nats.Connect("nats://localhost:4222")
+	if err != nil {
+		log.Fatalf("Error connecting to NATS: %s", err)
+	}
+
+	// Simple Publisher
+	nc.Publish("foo", []byte("Hello World"))
+
+	// Simple Async Subscriber
+	nc.Subscribe("foo", func(m *nats.Msg) {
+		fmt.Printf("Received a message: %s\n", string(m.Data))
+	})
+
+	nc.Close()
 }
--- src/worker/src/main.go
+++ src/worker/src/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"github.com/nats-io/nats.go"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	nc, _ := nats.Connect("nats://localhost:4222")
+
+	_, err := nc.QueueSubscribe("foo", "job_workers", func(message *nats.Msg) {
+		// Print the message
+		println(string(message.Data))
+	})
+	if err != nil {
+		log.Fatalf("Error subscribing to queue: %s", err)
+	}
+	nc.Close()
 }
