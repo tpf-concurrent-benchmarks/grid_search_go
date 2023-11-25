308f225853c82bf92084b905025f164661d817d2
ArmandoCivini <acivini@fi.uba.ar>
2023-11-25 14:55:07 -0300

    added partition test

--- src/manager/src/interval/partition_test.go
+++ src/manager/src/interval/partition_test.go
@@ -2,6 +2,7 @@ package interval
 
 import (
 	"testing"
+	"math"
 )
 
 func TestNewPartition(t *testing.T) {
@@ -39,5 +40,27 @@ func TestAvailable(t *testing.T) {
 
 	if !available {
 		t.Errorf("Expected Available to return true, got false")
+	}
+}
+
+func TestAllAvailable(t *testing.T) {
+	intervals := []Interval{
+		*NewInterval(0, 10, 5),
+		*NewInterval(0, 10, 5),
+		*NewInterval(0, 10, 5),
+	}
+	nIntervals := len(intervals)
+	maxChunkSize := 5
+
+	partition := NewPartition(intervals, nIntervals, maxChunkSize)
+	minBatches := int(math.Floor(float64(partition.fullCalculationSize())/float64(maxChunkSize))) + 1
+
+	for i := 0; i < minBatches; i++ {
+		available := partition.Available()
+
+		if !available {
+			t.Errorf("Expected Available to return true, got false")
+		}
+		partition.Next()
 	}
 }
