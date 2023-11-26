2e5d297574bfa3dae70b998d995ed4544fdee553
Facundo Mastricchio <facundomastricchio@gmail.com>
2023-11-26 01:01:52 -0300

    Add DTOs

--- src/common/dto/dtos.go
+++ src/common/dto/dtos.go
@@ -4,3 +4,18 @@ type WorkMessage struct {
 	Data [][3]float64 `json:"data"`
 	Agg  string       `json:"agg"`
 }
+
+type MaxResultsDTO struct {
+	Value      float64    `json:"value"`
+	Parameters [3]float64 `json:"parameters"`
+}
+
+type MinResultsDTO struct {
+	Value      float64    `json:"value"`
+	Parameters [3]float64 `json:"parameters"`
+}
+
+type AvgResultsDTO struct {
+	Value            float64 `json:"value"`
+	ParametersAmount uint64  `json:"paramsAmount"`
+}
